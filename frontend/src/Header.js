7e157fa7edafc07e84a6fa4c30634242e43bf514
cmd-accname <mariusbauer91@gmail.com>
2019-10-26 20:29:56 +0200

    asdasdasd

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Icon from '@material-ui/core/Icon';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+
 
 const useStyles = makeStyles(theme => ({
     grow: {
--- frontend/src/Pupils.js
+++ frontend/src/Pupils.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
 import Pupil from './Pupil';
-
-import {
-    Link,
-} from "react-router-dom";
 
 class Pupils extends React.Component
 {
     constructor(props) {
         super(props);
         this.state = {
-            users: [ {id : 1, name : "Test1"},
-                     {id : 1, name : "Test1"},
-                     {id : 1, name : "Test1"},
-                     {id : 1, name : "Test1"},
-                     {id : 1, name : "Test1"},
-                   ]
+            users: []
         };
     }
 
     componentDidMount()
     {
+        this.setState({
+            users: [
+                {"id" : 1, "name" : "Test1"},
+                {"id" : 1, "name" : "Test1"},
+                {"id" : 1, "name" : "Test1"},
+                {"id" : 1, "name" : "Test1"},
+                {"id" : 1, "name" : "Test1"},
+            ]
+        });
         /*fetch(new URL('init', "HIER MUSS EIN API CALL REIN")).then(res => res.json())
             .then((data => this.setState({
                                     users: data.users
@@ -35,11 +34,15 @@ class Pupils extends React.Component
 
     render()
     {
+
         return (
             <Grid container spacing={2}>
+                <Grid item lg={12}>
+                    <h1>Schüler</h1>
+                </Grid>
                 {this.state.users.map(values => (
                     <Grid item lg={this.props.size}>
-                        <Pupil id={values.id} name={values.name} />
+                        <Pupil name={values.name}/>
                     </Grid>
                 ))}
             </Grid>
