c7966095e4be556fc4154e219fa613ff4899f943
cmd-accname <mariusbauer91@gmail.com>
2019-10-27 02:52:55 +0100

    blubb

--- frontend/src/BNInput.js
+++ frontend/src/BNInput.js
@@ -174,7 +174,7 @@ class BNInput extends React.Component {
 
         // todo make more points of time selectable
         return (
-            <Container maxWidth="sm">
+            <Container maxWidth="lg" style={{background: "white"}}>
                     <table><tbody>
                     <tr>
                     <td>
